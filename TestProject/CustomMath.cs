bf185486ffa4da4360be53232a2fda5e04afeffe
Tofan Petru <tofanpetru12@gmail.com>
2022-12-18 19:32:01 +0200

    add project

---
+++ TestProject/CustomMath.cs
@@ -0,0 +1,33 @@
+﻿namespace TestProject
+{
+    [TestFixture]
+    public class CustomMath
+    {
+        [Test]
+        public void TestSum()
+        {
+            Console.WriteLine("TestSum");
+
+            int nbr1 = 0, nbr2 = 0;
+            int result = nbr1 + nbr2;
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(result == 0);
+                Assert.That(result, Is.EqualTo(0));
+            });
+        }
+
+        [Test, TestCaseSource(typeof(TestData), nameof(TestData.DivideByZeroDataProvider))]
+        public void TestDivisionByZero(int num1, int num2, double expected)
+        {
+            if (num1.Equals(0) || num2.Equals(0))
+            {
+                Assert.Throws<DivideByZeroException>(() => TestData.DivideByZeroNumbers(num1, num2));
+            }
+            else
+            {
+                Assert.That(expected, Is.EqualTo(TestData.DivideByZeroNumbers(num1, num2)));
+            }
+        }
+    }
+}
---
+++ TestProject/ExternalTestData/TestData.cs
@@ -0,0 +1,16 @@
+﻿namespace TestProject.ExternalTestData
+{
+    public static class TestData
+    {
+        public static int DivideByZeroNumbers(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static IEnumerable<TestCaseData> DivideByZeroDataProvider()
+        {
+            yield return new TestCaseData(2, 2, 1);
+            yield return new TestCaseData(3000, 0, 0);//will throw exception
+        }
+    }
+}
